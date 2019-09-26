3505b0820021dc2c33c2f08c252690cd13869439
marshal <marshal.wu@qq.com>
2019-09-26 21:17:47 +0800

    日志功能微调，更新readme

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ restful api server boilerplate.
 
 本项目逐步将从 helloworld 开始，逐步演化到可供开发者作为模板使用。
 
+## 基本特性
+
+- 使用 babel 支持 es6 语法
+
 ## 如何使用
+
+### 启动
 
 安装依赖包：
 
@@ -12,17 +18,39 @@ restful api server boilerplate.
 npm i
 ```
 
-启动开发环境：
+启动开发环境（通过 nodemon reload）：
+
+```
+npm run dev
+```
+
+启动生产环境
 
 ```
 npm start
 ```
 
-- 使用 nodemon 支持自动重新加载
-- 使用 babel 支持 es6 语法
+### 日志
+
+日志使用 winston。
+
+#### 开发环境
+
+开发环境，日志使用标准输出打印在屏幕。
+
+默认日志级别为 info，可通过配置文件./log/config.json 设置为 debug
+
+##### 生产环境
+
+日志均写入./log 目录下的对应日志文件。
+
+- ./app.log，一般的 info 级别的日志，供运维日常使用
+- ./error.log，仅输出 warn 级别以上日志，供运维日常使用
+- ./debug.log，默认不输出，需要 ./log/config.json 设置，供运维和开发诊断问题使用
 
 ## 版本历史
 
+- 0.0.4 实现日志功能，区分开发环境和生产环境
 - 0.0.3 支持 es6 语法，开发环境支持自动重新加载代码
 - 0.0.2 增加 koa-router 以及一个 CRUD 的示例
 - 0.0.1 helloworld
--- src/logger.js
+++ src/logger.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV == 'production') {
         {
             transports: [
                 new transports.File({ filename: './log/app.log', level: 'info' }),
-                new transports.File({ filename: './log/error.log', level: 'error' }),
+                new transports.File({ filename: './log/error.log', level: 'warn' }),
                 debugTransport
             ],
             format: format.simple()
@@ -38,47 +38,5 @@ if (process.env.NODE_ENV == 'production') {
         debugTransport.silent = !getConfig().debug
     });
 }
-
-// function getConfig() {
-//     let contents = fs.readFileSync("log/config.json")
-//     return JSON.parse(contents)
-// }
-
-// let setLoggerLevel
-// let debugTransport
-
-
-// const transportList = [
-// ]
-
-// if (process.env.NODE_ENV == 'development') {
-//     debugTransport = new transports.Console()
-//     transportList.push(debugTransport)
-//     setLoggerLevel = function (config) {
-//         if (config.debug) {
-//             debugTransport.level = 'debug'
-//         } else {
-//             debugTransport.level = 'info'
-//         }
-//     }
-// }
-
-// if (process.env.NODE_ENV == 'production') {
-//     transportList.push(new transports.File({ filename: './log/error.log', level: 'error' }))
-//     transportList.push(new transports.File({ filename: './log/app.log', level: 'info' }))
-// }
-
-
-// const logger = createLogger(
-//     {
-//         transports: transportList,
-//         format: format.simple()
-//     })
-
-// setLoggerLevel(logger, getConfig())
-
-// watch('log/config.json', { recursive: true }, function (event, name) {
-//     setLoggerLevel(logger, getConfig())
-// });
 
 export { logger }
